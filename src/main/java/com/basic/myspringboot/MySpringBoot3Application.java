42284d0b7d374fbe7c03eb93ad5803a0c43ef36d
24tngus <24tngus@naver.com>
2023-10-12 16:48:20 +0900

    네spring boot project initial setup

---
+++ src/main/java/com/basic/myspringboot/MySpringBoot3Application.java
@@ -0,0 +1,24 @@
+package com.basic.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class MySpringBoot3Application {
+
+	public static void main(String[] args) {
+
+		// SpringApplication.run(MySpringBoot3Application.class, args);
+		SpringApplication application = new SpringApplication(MySpringBoot3Application.class);
+		// WebApplication Type을 변경하기 위한 목적
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		// None : 더이상 WebApplication이 아님
+		application.run(args);
+	}
+
+
+}
---
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -0,0 +1,40 @@
+package com.basic.myspringboot.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(1)
+public class MyRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
+
+    @Value("${myboot.age}")
+    private int age;
+
+    @Value("${myboot.fullName}")
+    private String fullName;
+
+    @Autowired
+    private Environment environment;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // 포트 번호 받아오기
+        System.out.println("Port Number = " + environment.getProperty("local.server.port"));
+
+        // 환경변수 받아오기
+        System.out.println("myboot.name = " + name);
+        System.out.println("myboot.age = " + age);
+        System.out.println("myboot.fullName = " + fullName);
+
+        // ApplicationArguments는 main메서드의 (String[] args) argument를 전달 받음
+        System.out.println("VM Argument foo = " + args.containsOption("foo"));
+        System.out.println("Program argument bar = " + args.containsOption("bar"));
+    }
+}
