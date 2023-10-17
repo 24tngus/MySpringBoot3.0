a5097547aeaf8b11c0929001a4775b59e0ca6fb6
24tngus <24tngus@naver.com>
2023-10-17 13:04:05 +0900

    vegan_maven

---
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.basic.myspringboot.controller;
+
+import com.basic.myspringboot.dto.UserReqDTO;
+import com.basic.myspringboot.dto.UserResDTO;
+import com.basic.myspringboot.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/userspage")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/first")
+    public String leaf(Model model) {
+        model.addAttribute("name","스프링부트");
+        return "leaf";
+    }
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        List<UserResDTO> userResDTOList = userService.getUsers();
+        return new ModelAndView("index", "users", userResDTOList); // viewName, modelName, modelObject
+    }
+
+    // 등록 페이지를 호출 해주는 메서드
+    @GetMapping("/signup")
+    public String showSignUpForm(UserReqDTO user) {
+        return "add-user";
+    }
+    // 입력 항목 검증을 한 후 등록 처리 메서드
+    @PostMapping("/adduser")
+    public String addUser(@Valid UserReqDTO user, BindingResult result, Model model) {
+        // 입력 항목 검증 오류가 발생했는지 확인
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+
+        // 등록 요청
+        userService.saveUser(user);
+        model.addAttribute("users", userService.getUsers());
+        return "index";
+    }
+
+}
--- src/main/java/com/basic/myspringboot/dto/UserReqDTO.java
+++ src/main/java/com/basic/myspringboot/dto/UserReqDTO.java
@@ -1,5 +1,8 @@
 package com.basic.myspringboot.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +12,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter @Setter
 public class UserReqDTO {
+    @NotEmpty(message = "Name은 필수 입력 항목입니다") // " " 허용
     private String name;
+
+    @NotBlank(message = "Email은 필수 입력 항목입니다") // " " 허용하지 않음
+    @Email(message = "Email 형식이 아닙니다")
     private String email;
 }
